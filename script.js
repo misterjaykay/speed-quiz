423d5a55f753a18d83221ea402fbd15ca08fe722
Jonghyun Kang <misterjaykay@gmail.com>
2020-07-29 00:44:40 -0400

    added new javascript file, written a new object for questions/answers and new code lines for timer

--- script.js
+++ script.js
@@ -1,107 +1,70 @@
-var startButton = document.querySelector("#start");
-var timeEl = document.querySelector(".timer");
+var questionList = [{
+    question: "Q1: What is Tofu made of?",
+    answer: "Soy Beans",
+    choices: ["Milk", "Soy Beans", "Banana", "Red Beans"],
+    userAnswer: ""
+},
+{
+    question: "Q2: What type of food is Shiitake?",
+    answer: "Mushroom",
+    choices: ["Mushroom", "Jelly", "Vegetable", "Meat"],
+    userAnswer: ""
+},
+{
+    question: "Q3: Which word follows: Dijon, Wholegrain, and French?",
+    answer: "Mustard",
+    choices: ["Bread", "Mayonaisse", "Mustard", "Ketchup"],
+    userAnswer: ""
+},
+{
+    question: "Q4: Grape Seed, Sesame and Ground Nut are all types of what?",
+    answer: "Oil",
+    choices: ["Wine", "Oil", "Pasta", "Fruit"],
+    userAnswer: ""
+},
+{
+    question: "Q5: In which country was tea first grown?",
+    answer: "China",
+    choices: ["Mexico", "India", "France", "China"],
+    userAnswer: ""
+}];
 
-var quizTitle = document.querySelector(".questiontitle");
-var quizBody = document.querySelector(".questionbody");
-var mainEl = document.querySelector(".mainbox");
-var quizEl = document.querySelector(".quizbox");
 
 var points = 0;
+
+
+var startButton = document.getElementById("start");
+startButton.addEventListener("click", startQuiz);
+
+
+function startQuiz() {
+    console.log("start");
+    startTime();
+    showQuestion();
+}
+
+///// TIMER PART (OKAY) /////
+var timeEl = document.querySelector(".timer");
 var timeLeft = 60;
-timeEl.textContent = timeLeft + " sec Remaining";
+
 
 function startTime() {
-    startQuiz();
     timerInterval = setInterval(function() {
         timeLeft--;
         timeEl.textContent = timeLeft + " sec Remaining";
         if(timeLeft === 0) {
             clearInterval(timerInterval);
-            endMessage();
-         }
-     }, 1000);
+            // endMessage();
+        }
+    }, 1000);
 
 
 }
+//////////
 
-var answerList = document.querySelector(".answers");
-var answerA = document.querySelector("#answer-a");
-var answerB = document.querySelector("#answer-b");
-var answerC = document.querySelector("#answer-c");
-var answerD = document.querySelector("#answer-d");
-
-
-function startQuiz() {
-    
-    var answerRight = document.querySelector("#answer-c");
-    quizEl.children[1].style.display = "none";
-    mainEl.children[0].textContent = "Question 1";
-    mainEl.children[1].textContent = "Place your question 1 here?";
-    answerList.children[0].textContent = "Answer A";
-    answerList.children[0].style.display = "block";
-    answerList.children[1].textContent = "Answer B";
-    answerList.children[1].style.display = "block";
-    answerList.children[2].textContent = "Answer C";
-    answerList.children[2].style.display = "block";
-    answerList.children[3].textContent = "Answer D";
-    answerList.children[3].style.display = "block";
-
-    answerRight.addEventListener("click", function() {
-        alert("correct");
-        points++;
-        secondQuestion();
-    });
-}
-
-function secondQuestion() {
-    
-    var answerRight = document.querySelector("#answer-d");
-    // quizEl.children[1].style.display = "none";
-    mainEl.children[0].textContent = "Question 2";
-    mainEl.children[1].textContent = "Place your question 2 here?";
-    answerList.children[0].textContent = "Answer 1";
-    // answerList.children[0].style.display = "block";
-    answerList.children[1].textContent = "Answer 2";
-    // answerList.children[1].style.display = "block";
-    answerList.children[2].textContent = "Answer 3";
-    // answerList.children[2].style.display = "block";
-    answerList.children[3].textContent = "Answer 4";
-    // answerList.children[3].style.display = "block";
-
-    answerRight.addEventListener("click", function() {
-        alert("correct");
-        thirdQuestion();
-    });
-}
+///// LEAVING GLOBAL VARIABLES HERE /////
+var answerDiv = document.querySelector(".answer-div");
+var questionBody = document.querySelector(".questionbody")
+var questionIndex = 0;
 
 
-var thirdAnswer = ["Answer A", "Answer B", "Answer C", "Answer D"];
-
-function thirdQuestion() {
-    mainEl.children[0].textContent = "Question 3";
-    mainEl.children[1].textContent = "Place your question 3 here?";
-    answerList.children[0].textContent = thirdAnswer[0]; 
-    answerList.children[1].textContent = thirdAnswer[1];
-    answerList.children[2].textContent = thirdAnswer[2];
-    answerList.children[3].textContent = thirdAnswer[3]; 
-    
-    
-}
-
-
-
-function endMessage() {
-    mainEl.children[0].textContent = "Game Over!";
-    mainEl.children[1].textContent = "Thanks for playing. \nYour Score is " + points;
-    alert("60sec over");
-}
-
-startButton.addEventListener("click", startTime);
-
-// for loop to create a buttons for futre ref
-// for (var i = 0; i < answerOnes.length; i++) {
-//     var answerOne = answerOnes[i]
-//     var button = document.createElement("button");
-//     button.textContent = answerOne;
-//     answerList.appendChild(button);   
-// }
